23a5901f7519b118539bc766269239b74b2902f3
Ilene Cohen <ilene413@icloud.com>
2018-11-11 19:10:56 -0500

    onkeyproblem

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
-
+var numWins = 0;
+var numGuesses = 0;
+var maxNumGuesses = 14;
+var guessWords = ["chocolate", "vanilla", "sprinkles", "sundae", "cone", "cookies and cream", "fudge brownie", "banana split", "hot fudge", "thick Shake"];
+var lettersGuessed = new Array(14);
+lettersGuessed.fill (" ");
+function playHangman (startGame) {
+console.log (numWins);
+console.log (numGuesses);
+console.log (maxNumGuesses);
+console.log (guessWords);
+console.log (lettersGuessed);
+}
